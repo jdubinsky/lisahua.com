09aac2f3411a7a616f15ce5f1de40e5d43cab483
Jacob Dubinsky <jacobmdubinsky@gmail.com>
2019-09-28 18:48:59 -0400

    move tfstate bucket to terraform tf

--- infra/main.tf
+++ infra/main.tf
@@ -150,32 +150,3 @@ resource "aws_lambda_function" "lambda" {
     }
   }
 }
-
-
-resource "aws_s3_bucket" "tfstate" {
-  bucket = "lhua-tfstate"
-}
-
-resource "aws_s3_bucket_policy" "tfstate_policy" {
-  bucket = "${aws_s3_bucket.tfstate.id}"
-
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": "s3:ListBucket",
-      "Resource": "arn:aws:s3:::${aws_s3_bucket.tfstate.bucket}"
-    },
-    {
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": ["s3:GetObject", "s3:PutObject"],
-      "Resource": "arn:aws:s3:::${aws_s3_bucket.tfstate.bucket}/terraform.tfstate"
-    }
-  ]
-}
-POLICY
-}
--- infra/terraform.tf
+++ infra/terraform.tf
@@ -1,3 +1,31 @@
+resource "aws_s3_bucket" "tfstate" {
+  bucket = "lhua-tfstate"
+}
+
+resource "aws_s3_bucket_policy" "tfstate_policy" {
+  bucket = "${aws_s3_bucket.tfstate.id}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:ListBucket",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.tfstate.bucket}"
+    },
+    {
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": ["s3:GetObject", "s3:PutObject"],
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.tfstate.bucket}/terraform.tfstate"
+    }
+  ]
+}
+POLICY
+}
+
 terraform {
   backend "s3" {
     bucket = "lhua-tfstate"
