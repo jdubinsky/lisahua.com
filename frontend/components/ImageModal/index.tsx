11326634eb2e15942382b85d882cce415314c649
Jacob Dubinsky <jacob@ada.support>
2020-05-09 19:13:01 -0400

    img dynamic js scaling

--- frontend/components/ImageModal/index.tsx
+++ frontend/components/ImageModal/index.tsx
@@ -1,41 +1,84 @@
 import { h, Component, Fragment } from "preact";
-import { createPortal } from "preact/compat";
+import { createPortal, createRef } from "preact/compat";
 
 import CloseIcon from "../../icons/CloseIcon";
 
 import * as styles from "./styles";
 
 interface ImageModalProps {
-    isVisible: boolean;
-    onClose: () => void;
-    imageUrl: string;
+  isVisible: boolean;
+  onClose: () => void;
+  imageUrl: string;
 }
 
-export default class ImageModal extends Component<ImageModalProps, {}> {
-    static defaultProps = {
-        isVisible: false
-    };
+interface ImageModalState {
+  scale: number;
+  hasLoaded: boolean;
+}
 
-    render() {
-        if (!this.props.isVisible) {
-            return null;
-        }
+export default class ImageModal extends Component<
+  ImageModalProps,
+  ImageModalState
+> {
+  ref = createRef<HTMLImageElement>();
 
-        return createPortal(
-            <Fragment>
-                <styles.ModalOverlay />
-                <styles.ModalContainer>
-                    <styles.Modal>
-                        <styles.ModalActions>
-                            <styles.CloseButton onClick={this.props.onClose}>
-                                <CloseIcon></CloseIcon>
-                            </styles.CloseButton>
-                        </styles.ModalActions>
-                        <img src={this.props.imageUrl} />
-                    </styles.Modal>
-                </styles.ModalContainer>
-            </Fragment>,
-            document.body
-        );
+  static defaultProps = {
+    isVisible: false,
+  };
+
+  state = {
+    scale: 1,
+    hasLoaded: false,
+  };
+
+  setImageScale() {
+    if (!this.ref.current || this.state.hasLoaded) {
+      return;
     }
+
+    const browserWidth = window.innerWidth - 150;
+    const browserHeight = window.innerHeight - 150;
+    const imgWidth = this.ref.current.width;
+    const imgHeight = this.ref.current.height;
+    const scale = Math.min(browserWidth / imgWidth, browserHeight / imgHeight);
+    const newWidth = scale * imgWidth;
+    const newHeight = scale * imgHeight;
+    this.ref.current.height = newHeight;
+    this.ref.current.width = newWidth;
+    this.setState({ hasLoaded: true });
+  }
+
+  onLoad = () => {
+    if (this.ref.current) {
+      this.setImageScale();
+    }
+  };
+
+  render() {
+    if (!this.props.isVisible) {
+      return null;
+    }
+
+    return createPortal(
+      <Fragment>
+        <styles.ModalOverlay />
+        <styles.ModalContainer>
+          <styles.ModalActions>
+            <styles.CloseButton onClick={this.props.onClose}>
+              <CloseIcon></CloseIcon>
+            </styles.CloseButton>
+          </styles.ModalActions>
+          <styles.Modal>
+            <styles.ScaledImage
+              ref={this.ref}
+              src={this.props.imageUrl}
+              scale={this.state.scale}
+              onLoad={this.onLoad}
+            />
+          </styles.Modal>
+        </styles.ModalContainer>
+      </Fragment>,
+      document.body
+    );
+  }
 }
