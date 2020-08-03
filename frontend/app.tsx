ccde38c544fcab006f8e201c1216cd9064181cd8
Jacob Dubinsky <jacobmdubinsky@gmail.com>
2020-08-02 21:27:05 -0400

    404 page

--- frontend/app.tsx
+++ frontend/app.tsx
@@ -1,24 +1,14 @@
 import { h, render, Fragment } from "preact";
 import { BrowserRouter as Router, Switch, Redirect, Route } from "react-router-dom";
 
-import isMobile from "./is-mobile";
-
 import CanvassAICaseStudyPage from "./pages/CanvassAICaseStudy";
 import EsightCaseStudy from "./pages/EsightCaseStudy";
 import HomepageV2 from "./pages/HomepageV2";
 import MagnetCaseStudyPage from "./pages/MagentCaseStudyPage";
+import NotFound from "./pages/NotFound";
 import OnCallCaseStudy from "./pages/OnCallCaseStudy";
-import GoBackHeader from "./components/GoBackHeader";
 
 import GlobalFonts from "./globalStyles";
-
-function getMobileHeader() {
-  if (isMobile()) {
-    return <GoBackHeader />;
-  }
-
-  return <Fragment />;
-}
 
 function getApp() {
   return (
@@ -31,9 +21,7 @@ function getApp() {
           <Route path="/oncall" component={OnCallCaseStudy} />
           <Route path="/esight" component={EsightCaseStudy} />
           <Route exact path="/" component={HomepageV2} />
-          <Route path="/404">
-            <div />
-          </Route>
+          <Route path="/404" component={NotFound} />
           <Redirect to="/404" />
         </Switch>
       </Router>
