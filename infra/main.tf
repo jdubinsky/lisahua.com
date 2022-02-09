2226aacac4c431b176c1e10750ba4c7c1b70480d
Jacob Dubinsky <jacobmdubinsky@gmail.com>
2022-02-09 13:31:18 -0500

    use terraform cloud (#87)

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+
+  cloud {
+    organization = "jdubinsky-personal"
+
+    workspaces {
+      name = "lhua-github-workflow"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
